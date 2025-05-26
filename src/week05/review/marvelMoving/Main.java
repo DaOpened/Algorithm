d9b4b223304d8ef5514b089b5b8ef0ab059d3917
DaYeon <dayeon1211m@gmail.com>
2025-05-27 03:55:29 +0900

    week05.review.marvelMoving complete

--- src/week05/review/marvelMoving/Main.java
+++ src/week05/review/marvelMoving/Main.java
@@ -4,11 +4,8 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    // 상, 하, 좌, 우 이동
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-
     public static void main(String[] args) throws IOException{
+        // 미로찾기 => BFS 이용
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -19,7 +16,7 @@ class Main {
         char[][] board = new char[N][M];
         int rRow = 0, rCol = 0, bRow = 0, bCol = 0; // 구슬 위치
         int hRow = 0, hCol = 0; // 구멍 위치
-        int check = 0;
+
         // 보드 생성
         for(int i = 0 ; i < N ; i++) {
             String line = br.readLine();
@@ -40,7 +37,12 @@ class Main {
             }
         }
 
-        System.out.println(hRow+" "+hCol);
+        // 상, 하, 좌, 우 이동
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        String check = "0";
+
         Deque<List<Integer>> q = new ArrayDeque<>();
         boolean[][][][] visited = new boolean[N][M][N][M];
 
@@ -49,13 +51,15 @@ class Main {
 
         while(!q.isEmpty()) {
             List<Integer> cur = q.poll();
-            System.out.println("빨강 : ("+cur.get(0)+", "+cur.get(1)+") 파랑 : ("+cur.get(2)+", "+cur.get(3)+") "+cur.get(4));
 
-            // 빨간 구슬이 구멍에 도달한 경우
+            // 시도 횟수가 10회 초과한 경우 실패
+            if(cur.get(4) > 10) {
+                break;
+            }
+
+            // 빨간 구슬이 구멍에 도달한 경우 성공
             if(cur.get(0) == hRow && cur.get(1) == hCol) {
-                //bw.write(1);
-                check = 1;
-                System.out.println("구멍도달");
+                check = "1";
                 break;
             }
 
@@ -63,15 +67,18 @@ class Main {
             int cBluex = cur.get(2), cBluey = cur.get(3);
 
             // 1. 두 구슬 동시에 이동 가능한 곳까지 같이 이동
-            // 2. 파란 구슬이 구멍에 다다르면 다른 방향으로
-            // 3. 더 갈 수 있는 구슬은 더 보냄
+            // 2. 빨간 구슬 이동 가능한 곳까지 이동 (구멍포함)
+            // 3. 파란 구슬 이동 가능한 곳까지 이동
+                // 3-1. 파란 구슬 구멍 도달시 실패 => 다음 탐색
+            // 4. 방문한 적이 없다면 큐에 추가
             for(int i = 0; i < 4 ; i++) {
-                // 구슬 이동 가능한 경우
-                if(board[cRedx+dx[i]][cRedy+dy[i]] == '.' || board[cBluex+dx[i]][cBluey+dy[i]] == '.') {
+                // 구슬 이동 가능한 경우 (둘다 벽만 아니면 됨)
+                if(board[cRedx+dx[i]][cRedy+dy[i]] != '#' || board[cBluex+dx[i]][cBluey+dy[i]] != '#') {
                     int nRedx = cRedx;
                     int nRedy = cRedy;
                     int nBluex = cBluex;
                     int nBluey = cBluey;
+
                     // 1. 두 구슬 동시에 이동 가능한 곳까지 같이 이동
                     while (board[nRedx + dx[i]][nRedy + dy[i]] == '.' && board[nBluex + dx[i]][nBluey + dy[i]] == '.') {
                         nRedx += dx[i];
@@ -80,51 +87,49 @@ class Main {
                         nBluey += dy[i];
                     }
 
-                    // 2. 파란 구슬이 구멍에 다다르면 다른 방향으로
-                    if(board[nBluex+dx[i]][nBluey+dy[i]] == 'O') {
-                        //System.out.println("파랑 구멍");
-                        continue;
-                    }
-
-                    if(board[nRedx+dx[i]][nRedy+dy[i]] == 'O') {
-                        //check = 1;
-                        System.out.println("빨강 구멍 임박");
-                        nRedx+=dx[i];
-                        nRedy+=dy[i];
-                    }
-
-                    // 3. 더 갈 수 있는 구슬은 더 보냄
-                    // 3-1. 빨간 구슬이 더 갈 수 있는 경우
-                    if(board[nRedx+dx[i]][nRedy+dy[i]] == '.') {
+                    // 2. 빨간 구슬 이동 가능한 곳까지 이동 (구멍포함)
+                    if(board[nRedx+dx[i]][nRedy+dy[i]] != '#') {
+                        char tmp = board[nBluex][nBluey];
                         board[nBluex][nBluey] = 'B';
                         while(board[nRedx+dx[i]][nRedy+dy[i]] == '.') {
                             nRedx += dx[i];
                             nRedy += dy[i];
                         }
-                        board[nBluex][nBluey] = '.';
+                        board[nBluex][nBluey] = tmp; // 보드 복구
+
+                        // 2-1. 빨간 구슬이 구멍에 들어감
+                        if((nRedx+dx[i] == hRow) && (nRedy+dy[i] == hCol)) {
+                            nRedx += dx[i];
+                            nRedy += dy[i];
+                        }
                     }
 
-                    // 3-2. 파란 구슬이 더 갈 수 있는 경우
-                    else if(board[nBluex+dx[i]][nBluey+dy[i]] == '.') {
+                    // 3. 파란 구슬 이동 가능한 곳까지 이동
+                    if(board[nBluex+dx[i]][nBluey+dy[i]] != '#') {
+                        char tmp = board[nRedx][nRedy];
                         board[nRedx][nRedy] = 'R';
                         while(board[nBluex+dx[i]][nBluey+dy[i]] == '.') {
                             nBluex += dx[i];
                             nBluey += dy[i];
                         }
-                        board[nRedx][nRedy] = '.';
+                        board[nRedx][nRedy] = tmp;
+
+                        // 3-1. 파란 구슬 구멍 도달시 실패 => 다음 탐색
+                        if((nBluex+dx[i] == hRow) && (nBluey+dy[i] == hCol)) {
+                            continue;
+                        }
                     }
 
-                    if (cur.get(4) < 10 && !visited[nRedx][nRedy][nBluex][nBluey]) {
+                    // 4. 방문한 적이 없다면 큐에 삽입
+                    if (!visited[nRedx][nRedy][nBluex][nBluey]) {
                         q.offer(Arrays.asList(nRedx, nRedy, nBluex, nBluey, cur.get(4) + 1));
-                        //System.out.println("빨강 : ("+nRedx+", "+nRedy+") 파랑 : ("+nBluex+", "+nBluey+")");
                         visited[nRedx][nRedy][nBluex][nBluey] = true;
                     }
                 }
             }
         }
-        //bw.write(1);
-        System.out.println(check);
-        //bw.flush();
+        bw.write(check);
+        bw.flush();
     }
 }
 
